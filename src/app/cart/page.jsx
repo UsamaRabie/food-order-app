fc7220fe08ebf2d309fb95f5b12b5b87a9b5149c
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-14 22:09:07 +0300

    Update page.jsx

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useSelector , useDispatch} from 'react-redux'
 import { deleteFromCart , clearCart } from "../redux_toolkit/slices/cart/cartSlice";
 const CartPage = () => {
-  const cart = useSelector(state=>state.cart)
+   const cart = localStorage.getItem("cartItems")
   const dispatch = useDispatch()
   let myPrice = 0;
   let myQuantity = 0;
